f7bb88d33eabdd51e4826a14ddd7d4cfcab146fc
YoramMengistu <119293009+YoramMengistu@users.noreply.github.com>
2024-12-07 12:45:16 +0200

    update-mongo-timeout

--- server/app.js
+++ server/app.js
@@ -150,7 +150,11 @@ app.use(express.json());
 app.use("/api/codeblocks", require("./routes/codeBlockRoute"));
 
 mongoose
-  .connect(process.env.MONGO_URI)
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 5000, // הגדרת timeout ל-5 שניות
+  })
   .then(() => {
     console.log("MongoDB Connected");
   })
