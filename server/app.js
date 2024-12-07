f5f0b71363bd4096b5f9007ed9864462b382cfd8
YoramMengistu <119293009+YoramMengistu@users.noreply.github.com>
2024-12-07 15:40:05 +0200

    update

--- server/app.js
+++ server/app.js
@@ -11,7 +11,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "https://code-block-client.netlify.app/",
+    origin: ["http://localhost:5174", "https://code-block-client.netlify.app"],
     methods: ["GET", "POST"],
   },
 });
